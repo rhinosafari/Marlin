9ccd2a8b03eb25fddad6a3f50a646fd195d04a6c
Nick Kohler <nick.kohler@junipersys.com>
2021-02-28 10:56:27 -0700

    Revert preheat fan speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1690,13 +1690,11 @@
 #define PREHEAT_1_TEMP_HOTEND 200
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
  * Nozzle Park
